8f33b4f5b36103bba771f716019b421ea6402819
juansncz <johnfrancisvolante@gmail.com>
2024-12-13 04:28:32 +0800

    login.jsx src/Register.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -9,7 +9,7 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import { API_ENDPOINT } from './api';
+import { API_ENDPOINT } from './api.jsx';
 
 function Login() {
   const navigate = useNavigate();
