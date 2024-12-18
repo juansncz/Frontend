585f9b1a86916642ff196f4e0602e4ddfbaa5547
juansncz <johnfrancisvolante@gmail.com>
2024-12-18 23:05:32 +0800

    updateee

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
   build: {
+    sourcemap: true,
     rollupOptions: {
-      external: [], // Ensure all dependencies are bundled
+      external: [],
     },
+  },
+  define: {
+    'process.env': process.env,
   },
 });
